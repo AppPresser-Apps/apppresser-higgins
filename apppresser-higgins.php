0f427c3c9d551008226af4d7a244a1672f4b2123
modemlooper <modemlooper@gmail.com>
2022-04-11 09:27:50 -0700

    first commit

---
+++ apppresser-higgins.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * AppPresser
+ *
+ * @package   AppPresser
+ * @copyright Copyright(c) 2019, AppPresser LLC
+ * @license http://opensource.org/licenses/GPL-2.0 GNU General Public License, version 2 (GPL-2.0)
+ *
+ * Plugin Name: AppPresser Custom
+ * Plugin URI: https://apppresser.com
+ * Description: Functionality for custom AppPresser Apps.
+ * Version: 1.0.0
+ * Author: AppPresser
+ * Author URI: https://apppresser.com
+ * License: GPL2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: apppresser
+ * Domain Path: languages
+ */
+
+define( 'APPPRESSER_VERSION', '1.0.0' );
+define( 'APPPRESSER_PLUGIN_NAME', 'AppPresser' );
+define( 'APPPRESSER_DIR', plugin_dir_path( __FILE__ ) );
+define( 'APPPRESSER_URL', plugins_url( '/', __FILE__ ) );
+define( 'APPPRESSER_SLUG', plugin_basename( __FILE__ ) );
+define( 'APPPRESSER_FILE', __FILE__ );
+
+require dirname( __FILE__ ) . '/class-apppresser.php';
+
+/**
+ * The main function.
+ *
+ * @return AppPresser|null The one true AppPresser Instance.
+ */
+function apppresser() {
+	return AppPresser::instance();
+}
+add_action( 'plugins_loaded', 'apppresser' );
+
+
+
+add_action(
+	'rest_api_init',
+	function() {
+
+		remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+
+		add_filter( 'rest_pre_serve_request', 'initCors' );
+	},
+	15
+);
+
+
+function initCors( $value ) {
+	$origin_url = '*';
+
+	header( 'Access-Control-Allow-Origin: ' . $origin_url );
+	header( 'Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS' );
+	header( 'Access-Control-Allow-Credentials: true' );
+	return $value;
+}
---
+++ class-apppresser.php
@@ -0,0 +1,73 @@
+<?php
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Main AppPresser Class.
+ *
+ * @since 1.0.0
+ */
+class AppPresser {
+
+
+	/**
+	 * Main AppPresser Instance.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @static object $instance
+	 * @see AppPresser()
+	 *
+	 * @return AppPresser|null The one true AppPresser.
+	 */
+	public static function instance() {
+
+		// Store the instance locally to avoid private static replication.
+		static $instance = null;
+
+		// Only run these methods if they haven't been run previously.
+		if ( null === $instance ) {
+			$instance = new AppPresser();
+			$instance->vendors();
+			$instance->includes();
+		}
+
+		// Always return the instance.
+		return $instance;
+
+		// Long live and prosper.
+	}
+
+	/** Magic Methods *********************************************************/
+
+	/**
+	 * A dummy constructor to prevent AppPresser from being loaded more than once.
+	 *
+	 * @since 1.0.0
+	 * @see AppPresser::instance()
+	 * @see AppPresser()
+	 */
+	private function __construct() {
+		/* Do nothing here */
+	}
+
+	/**
+	 * Include files
+	 *
+	 * @return void
+	 */
+	public function includes() {
+		require_once APPPRESSER_DIR . '/includes/functions.php';
+	}
+
+	/**
+	 * Include vendors
+	 *
+	 * @return void
+	 */
+	public function vendors() {
+		require_once APPPRESSER_DIR . 'vendors/jwt-authentication/jwt-authentication.php';
+	}
+
+}
